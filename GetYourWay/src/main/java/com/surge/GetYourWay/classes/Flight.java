c5c3f01118ff76c69938aaf6d6a22ce68da24f56
Sam <sam.skinner@sky.uk>
2022-07-18 16:57:10 +0100

    Added flight class

---
+++ GetYourWay/src/main/java/com/surge/GetYourWay/classes/Flight.java
@@ -0,0 +1,57 @@
+package com.surge.GetYourWay.classes;
+
+public class Flight {
+
+    private int flightId;
+
+    private String origin;
+
+    private String destinationId;
+
+    private LocalDateTime departTime;
+
+    private LocalDateTime arriveTime;
+
+    public Flight() {
+    }
+
+    public int getFlightId() {
+        return flightId;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public String getDestinationId() {
+        return destinationId;
+    }
+
+    public LocalDateTime getDepartTime() {
+        return departTime;
+    }
+
+    public LocalDateTime getArriveTime() {
+        return arriveTime;
+    }
+
+    public void setFlightId(int flightId) {
+        this.flightId = flightId;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public void setDestinationId(String destinationId) {
+        this.destinationId = destinationId;
+    }
+
+    public void setDepartTime(LocalDateTime departTime) {
+        this.departTime = departTime;
+    }
+
+    public void setArriveTime(LocalDateTime arriveTime) {
+        this.arriveTime = arriveTime;
+    }
+}
