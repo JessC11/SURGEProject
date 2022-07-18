d230f18932a6814b2935b79bfb174decb528a903
Jessica Cawdron <jessica.cawdron@sky.uk>
2022-07-18 16:41:50 +0100

    Adding the destination class and destination repository

---
+++ GetYourWay/src/main/java/com/surge/GetYourWay/classes/Destination.java
@@ -0,0 +1,36 @@
+package com.surge.GetYourWay.classes;
+
+public class Destination {
+    private int destinationId;
+    private String destination;
+    private String programme;
+
+
+
+    public int getDestinationId() {
+        return destinationId;
+    }
+
+    public void setDestinationId(int destinationId) {
+        this.destinationId = destinationId;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getProgramme() {
+        return programme;
+    }
+
+    public void setProgramme(String programme) {
+        this.programme = programme;
+    }
+}
+
+
+
