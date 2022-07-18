d7200f5dd99ca8746a484123834fd9dbebc7151c
Emma Naylor <emma.naylor@sky.uk>
2022-07-18 16:40:13 +0100

    Created basic trip and journey classes to match with DB design

---
+++ GetYourWay/src/main/java/com/surge/GetYourWay/Journey.java
@@ -0,0 +1,32 @@
+package com.surge.GetYourWay;
+
+public class Journey {
+    private int journeyId;
+    private int flightId;
+    private int tripId;
+
+
+    public int getJourneyId() {
+        return journeyId;
+    }
+
+    public void setJourneyId(int journeyId) {
+        this.journeyId = journeyId;
+    }
+
+    public int getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId(int flightId) {
+        this.flightId = flightId;
+    }
+
+    public int getTripId() {
+        return tripId;
+    }
+
+    public void setTripId(int tripId) {
+        this.tripId = tripId;
+    }
+}
