46e9dd20a0a1a4bfd7420e5872fab2747d6bb761
Emma Naylor <emma.naylor@sky.uk>
2022-07-20 17:04:33 +0100

    Updated react navbar

--- GetYourWay/app/src/App.js
+++ GetYourWay/app/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-import Searching from './views/Searching';
+import './navbar.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navibar from './components/Navbar';
+import Searching from './pages/Searching';
+import backgr from './img/homepageimg.jpeg';
 
 const App = () => {
   return (
     <div className="App">
+      <header>
+      <Navibar/>
+      </header>
+      <body>
       <Searching/>
+      </body>
     </div>
   );
 }
---
+++ GetYourWay/app/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from '../surgelogo6.png';
+
+function Navibar() {
+  return (
+        <Navbar className='nav' expand="lg" variant="light">
+        <Container className='navbar' fluid>
+          <Navbar.Brand href="/"><img src={logo} class="img-fluid" id="nav-logo"/></Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/home">Login</Nav.Link>
+            <Nav.Link href="/searching">My Search</Nav.Link>
+            <Nav.Link href="/destination">Destination</Nav.Link>
+            <Nav.Link href="/journey">My Journey</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+  );
+}
+
+export default Navibar;
