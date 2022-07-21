7fccd113b920fecda2a8f2c426e6d5b7c7106d96
Sam <sam.skinner@sky.uk>
2022-07-21 14:08:30 +0100

    Added methods to set up controller actions ready for front end

---
+++ GetYourWay/src/main/java/com/surge/GetYourWay/Domain/dao/viewModelRepository/TripViewModelRepository.java
@@ -0,0 +1,2 @@
+package com.surge.GetYourWay.domain.dao.viewModelRepository;public class TripViewModelRepository {
+}
---
+++ GetYourWay/src/main/java/com/surge/GetYourWay/viewModel/TripViewModel.java
@@ -0,0 +1,2 @@
+package com.surge.GetYourWay.viewModel;public class TripViewModel {
+}
