3814ff451728f96b709e5a4a8237dd11a8228437
Ryan Brennan <ryan.brennan@sky.uk>
2022-07-22 16:16:56 +0100

    links to social media login for sharing travel plans

--- GetYourWay/app/src/components/FlightItem.js
+++ GetYourWay/app/src/components/FlightItem.js
@@ -1,35 +1,34 @@
 import '../App.css';
 import departLogo from '../img/departures.png';
 import arriveLogo from '../img/landing.png';
+import facebookLogo from '../img/facebook.png';
+import instagramLogo from '../img/instagram.png';
+import twitterLogo from '../img/twitter.png';
 
 const FlightItem = ({departure, departTime, arrival, arrivalTime}) => {
 
-//<div className="flight">
-//                <div className="departContainer">
-//                    <p className="departTime">{departTime}</p>
-//                </div>
-//                <img className="departImg" src={departLogo}/>
-//                <p className="departure">{departure}</p>
-//                <p className="departure">></p>
-//                <p className="arrival">{arrival}</p>
-//                <img className="arriveImg" src={arriveLogo}/>
-//                <div className="arriveContainer">
-//                    <p className="arrivalTime">{arrivalTime}</p>
-//                </div>
-//            </div>
-//
-//            <div className="addFlight">
-//
-//            </div>
+    function openURL(url) {
+        window.open(url, '_blank', 'noopener,noreferrer');
+    }
+
     return (
-        <div className="flightContainer">
-            <p className="departTime">{departTime}</p>
-            <img className="departImg" src={departLogo}/>
-            <p className="departure">{departure}</p>
-            <p className="symbol">></p>
-            <p className="arrive">{arrival}</p>
-            <img className="arriveImg" src={arriveLogo}/>
-            <p className="arriveTime">{arrivalTime}</p>
+        <div>
+            <div className="flightContainer">
+                <p className="departTime">{departTime}</p>
+                <img className="departImg" src={departLogo}/>
+                <p className="departure">{departure}</p>
+                <p className="symbol">></p>
+                <p className="arrive">{arrival}</p>
+                <img className="arriveImg" src={arriveLogo}/>
+                <p className="arriveTime">{arrivalTime}</p>
+            </div>
+
+            <div className="sharingContainer">
+                <h3>Share Your Surge Travel Plans</h3>
+                   <img className="facebookImg" src = {facebookLogo} onClick={() => openURL('https://facebook.com')}/>
+                   <img className="instagramImg" src = {instagramLogo} onClick={() => openURL('https://instagram.com')}/>
+                   <img className="twitterImg" src = {twitterLogo} onClick={() => openURL('https://twitter.com')}/>
+            </div>
         </div>
     )
 
