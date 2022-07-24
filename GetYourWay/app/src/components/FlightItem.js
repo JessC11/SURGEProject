3272b2e60dc9b1f7912562a36b373ecbbe5083a0
Ryan Brennan <ryan.brennan@sky.uk>
2022-07-24 14:27:28 +0100

    style changes to add to journey button

--- GetYourWay/app/src/components/FlightItem.js
+++ GetYourWay/app/src/components/FlightItem.js
@@ -2,33 +2,43 @@ import '../App.css';
 import departLogo from '../img/departures.png';
 import arriveLogo from '../img/landing.png';
 
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import IconButton from '@mui/material/IconButton';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+
+import {useState} from 'react';
+
 const FlightItem = ({departure, departTime, arrival, arrivalTime, index, isBooking}) => {
+
+    const [isAdded, setIsAdded] = useState(false);
 
     // TODO: need times in DateTime, need persistent user info
     function onAdd() {
 
-        let id = 1;
-        const flight = {
-            origin: departure,
-            departTime: departTime,
-            arrive: arrival,
-            arriveTime: arrivalTime
-        }
-        const requestOptions = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': '*'
-            },
-            body: JSON.stringify(flight)
-        };
-        let address = 'http://localhost:8080/flights/' + id;
-        fetch(address , requestOptions)
-        .then(response => response.json())
-        .then(result=>{
-            console.log(result)
-        })
+        setIsAdded((prev) => !prev);
+
+//        let id = 1;
+//        const flight = {
+//            origin: departure,
+//            departTime: departTime,
+//            arrive: arrival,
+//            arriveTime: arrivalTime
+//        }
+//        const requestOptions = {
+//            method: 'POST',
+//            headers: {
+//                'Content-Type': 'application/json',
+//                'Access-Control-Allow-Origin': '*',
+//                'Access-Control-Allow-Headers': '*'
+//            },
+//            body: JSON.stringify(flight)
+//        };
+//        let address = 'http://localhost:8080/flights/' + id;
+//        fetch(address , requestOptions)
+//        .then(response => response.json())
+//        .then(result=>{
+//            console.log(result)
+//        })
     }
 
     let d = new Date(departTime);
@@ -45,9 +55,13 @@ const FlightItem = ({departure, departTime, arrival, arrivalTime, index, isBooki
                 <p className="arrive">{arrival}</p>
                 <p className="arriveTime">{arrTime}</p>
             </div>
-            {isBooking && <div onClick={onAdd} style={{gridColumn: '2', gridRow: index, height: '75px', width: '75px', display: 'grid', alignItems: 'center'}}>
-                <p className="addText">Add Me to Your Journey</p>
-            </div>}
+
+            {isBooking &&
+                <IconButton onClick={onAdd} aria-label="add" style={{gridColumn: '2', gridRow: index, height: '50px', width: '50px'}}>
+                    {!isAdded && <AddCircleOutlineIcon style={{height: '40px', width: '40px'}}/>}
+                    {isAdded && <CheckCircleOutlineIcon style={{height: '40px', width: '40px'}}/>}
+                  </IconButton>
+            }
         </>
     )
 
